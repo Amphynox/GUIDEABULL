1cd77919262e8f58d9d0debe53094af73d6e0c49
Amphynox <53525927+Amphynox@users.noreply.github.com>
2020-04-03 00:25:23 -0400

    Initial commit

---
+++ app/src/main/java/com/example/guideabull/activity_buddy.kt
@@ -0,0 +1,12 @@
+package com.example.guideabull
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_buddy : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buddy)
+    }
+}
---
+++ app/src/main/java/com/example/guideabull/activity_navigation.kt
@@ -0,0 +1,12 @@
+package com.example.guideabull
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_navigation : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation)
+    }
+}
